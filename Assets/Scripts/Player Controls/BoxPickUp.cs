39162343ae41171d9b8da13a30b5de87eefb4bff
Oğuzhan Arar <ichbinizm@gmail.com>
2020-07-30 05:00:41 +0300

    Player Picking up Boxes and Stacking them under itself.

---
+++ Assets/Scripts/Player Controls/BoxPickUp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxPickUp : MonoBehaviour
+{
+  private Player player;
+  void Start(){
+    player = FindObjectOfType<Player>();
+  }
+  private void OnTriggerEnter(Collider other)
+      {
+          //Check to see if the tag on the collider is equal to Enemy
+          if (other.tag == "BoxPickupable")
+          {
+              player.addBox();
+              Destroy(other);
+          }
+      }
+}
---
+++ Assets/Scripts/Player Controls/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public List<GameObject> Boxes = new List<GameObject>();
+    [SerializeField]private GameObject Box;
+    [SerializeField]private float size = 1f, offset = 0.2f, startHeight = -0.5f;
+
+    void Start(){
+      if(Boxes.Count > 0){
+        BoxRearrange();
+      }
+    }
+
+    public void addBox(){
+      GameObject BoxClone = Instantiate(Box, Vector3.zero, Quaternion.identity) as GameObject;
+      Boxes.Add(BoxClone);
+      BoxRearrange();
+    }
+
+    public void BoxRearrange(){
+        gameObject.transform.position = new Vector3(transform.position.x, (transform.position.y)+(Boxes.Count)*(size+offset),transform.position.z);
+        int i = 1;
+
+        foreach(GameObject reBox in Boxes){
+          reBox.transform.position = new Vector3(transform.position.x, transform.position.y+startHeight-(i*(size+offset)),transform.position.z);
+          i++;
+        }
+    }
+
+}
